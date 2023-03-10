f1396f9ebf0bfed01c47bb95c2aa9599fa29ebf5
omarhossam110 <m.omar.hossam@gmail.com>
2023-03-11 01:46:19 +0200

    Responsive edits

--- src/components/About.js
+++ src/components/About.js
@@ -49,9 +49,9 @@ const About = () => {
                       <svg className={`about__info-svg`} viewBox="0 0 448 512">
                         <path d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z" />
                       </svg>
-                      Date of birth:
+                      Birthday:
                     </span>
-                    <span className={`about__info-span col-xs-4`}>1/8/1998</span>
+                    <span className={`about__info-span  `}>1/8/1998</span>
                   </div>
 
                   <div className={`about__info-description`}>
@@ -61,7 +61,7 @@ const About = () => {
                       </svg>
                       Address:
                     </span>
-                    <span className={`about__info-span col-xs-4`}>Cairo, Egypt</span>
+                    <span className={`about__info-span  `}>Cairo, Egypt</span>
                   </div>
 
                   <div className={`about__info-description`}>
@@ -71,7 +71,7 @@ const About = () => {
                       </svg>
                       Freelance:
                     </span>
-                    <span className={`about__info-span col-xs-4`}>Available</span>
+                    <span className={`about__info-span  `}>Available</span>
                   </div>
 
                   <div className={`about__info-description`}>
@@ -79,9 +79,9 @@ const About = () => {
                       <svg className={`about__info-svg`} viewBox="0 0 512 512">
                         <path d="M280 0C408.1 0 512 103.9 512 232c0 13.3-10.7 24-24 24s-24-10.7-24-24c0-101.6-82.4-184-184-184c-13.3 0-24-10.7-24-24s10.7-24 24-24zm8 192a32 32 0 1 1 0 64 32 32 0 1 1 0-64zm-32-72c0-13.3 10.7-24 24-24c75.1 0 136 60.9 136 136c0 13.3-10.7 24-24 24s-24-10.7-24-24c0-48.6-39.4-88-88-88c-13.3 0-24-10.7-24-24zM117.5 1.4c19.4-5.3 39.7 4.6 47.4 23.2l40 96c6.8 16.3 2.1 35.2-11.6 46.3L144 207.3c33.3 70.4 90.3 127.4 160.7 160.7L345 318.7c11.2-13.7 30-18.4 46.3-11.6l96 40c18.6 7.7 28.5 28 23.2 47.4l-24 88C481.8 499.9 466 512 448 512C200.6 512 0 311.4 0 64C0 46 12.1 30.2 29.5 25.4l88-24z" />
                       </svg>
-                      Phone Number:
+                      Phone:
                     </span>
-                    <span className={`about__info-span col-xs-4`}>01004938289</span>
+                    <span className={`about__info-span  `}>01004938289</span>
                   </div>
                 </div>
               </div>
